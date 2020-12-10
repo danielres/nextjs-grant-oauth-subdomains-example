f1887a3e5dcbd974da13ea2ea22e69429a1b150c
Daniel Reszka <dannn.r@gmail.com>
2020-12-10 13:20:47 +0100

    auth: setup config

---
+++ pages/api/config.ts
@@ -0,0 +1,20 @@
+export const cookieSession = {
+  maxAge: 2 * 60e3,
+  name: "session",
+  secret: process.env.COOKIE_SESSION_SECRET,
+};
+
+export const grant = {
+  defaults: {
+    origin: process.env.OAUTH_DEFAULTS_ORIGIN,
+    transport: "session",
+    state: true,
+  },
+  google: {
+    key: process.env.OAUTH_GOOGLE_KEY,
+    secret: process.env.OAUTH_GOOGLE_SECRET,
+    scope: ["openid", "email", "profile"],
+    response: ["tokens", "profile"],
+    callback: "/api/redirect",
+  },
+};
